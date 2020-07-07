acd79ebcbc6bb6e424719f79870515931f347694
Meg Hsu <meghsu@Megs-MBP-2.localdomain>
2020-07-06 18:49:03 -0700

    Chapter 6 Final Version

--- Chapter_6_Lab/Chapter_6_Lab.cpp
+++ Chapter_6_Lab/Chapter_6_Lab.cpp
@@ -1,112 +1,188 @@
-// Name: Meg Hsu
-// Description: This is a program that allows you to convert time in twelve and twenty four hour notation into other notations of time.
-// Course: CS 102 - Introduction to Programming C++
+// Name: Chapter_6_Lab (Converting Time)
+// Author: Meg Hsu
 // Due Date: 6 July 2020
+// Description: This is a program that allows you to convert time in twelve and twenty four hour notation into other notations of time.
 
 #include <iostream>
 #include<iomanip>
-#include <time.h>
-//using namespace std;
+using namespace std;
 
-void output12(int hour, int minute, int second, std::string am_pm) {
+//Function Name: output12
+//Purpose: The purpose of this function is to output the original time and the time that has been converted as well.
+//Parameter: hours, minutes, seconds, and AM or PM
+//Return: n/a
+
+//setfill and setw are used for formatting the output in hh:mm:ss format.
+void output12(int hour, int minute, int second, string am_pm) {
 	if (am_pm == "am" or am_pm == "AM" or am_pm == "Am") {
-		std::cout << "The time " << hour << ":" << minute << ":" << second
-				<< " " << am_pm << " in twelve hour format is " << hour << ":"
-				<< minute << ":" << second << " in twenty four hour format.";
+		cout << "The time " << setfill('0') << setw(2) << hour
+				<< ":" << setfill('0') << setw(2) << minute << ":"
+				<< setfill('0') << setw(2) << second << " " << am_pm
+				<< " in 12-hour format is " << setfill('0')
+				<< setw(2) << hour << ":" << setfill('0')
+				<< setw(2) << minute << ":" << setfill('0')
+				<< setw(2) << second << " in 24-hour format.";
 	}
 	if (am_pm == "pm" or am_pm == "PM" or am_pm == "Pm") {
-		std::cout << "The time " << hour << ":" << minute << ":" << second
-				<< " " << am_pm << " in twelve hour format is " << (hour + 12)
-				<< ":" << minute << ":" << second
-				<< " in twenty four hour format.";
+		cout << "The time " << setfill('0') << setw(2) << hour
+				<< ":" << setfill('0') << setw(2) << minute << ":"
+				<< setfill('0') << setw(2) << second << " " << am_pm
+				<< " in 24-hour format is " << setfill('0')
+				<< setw(2) << (hour + 12) << ":" << setfill('0')
+				<< setw(2) << minute << ":" << setfill('0')
+				<< setw(2) << second << " in 24-hour format.";
 	}
 }
+
+//Function Name: output24
+//Purpose: The purpose of this function is to output the original time and the time that has been converted as well.
+//Parameter: hours, minutes, and seconds
+//Return: n/a
 
 void output24(int hour, int minute, int second) {
 	if (hour > 12) {
-		std::cout << "The time " << hour << ":" << minute << ":" << second
-				<< " in twenty four hour format is " << (hour - 12) << ":"
-				<< minute << ":" << second << " PM in twelve four hour format.";
+		cout << "The time " << setfill('0') << setw(2) << hour
+				<< ":" << setfill('0') << setw(2) << minute << ":"
+				<< setfill('0') << setw(2) << second
+				<< " in 24-hour format is " << setfill('0')
+				<< setw(2) << (hour - 12) << ":" << setfill('0')
+				<< setw(2) << minute << ":" << setfill('0')
+				<< setw(2) << second << " PM in 12-hour format.";
 	}
 	if (hour <= 12) {
-		std::cout << "The time " << hour << ":" << minute << ":" << second
-				<< " in twenty four hour format is " << (hour - 12) << ":"
-				<< minute << ":" << second << " AM in twelve hour format.";
+		cout << "The time " << setfill('0') << setw(2) << hour
+				<< ":" << setfill('0') << setw(2) << minute << ":"
+				<< setfill('0') << setw(2) << second
+				<< " in 24-hour format is " << setfill('0')
+				<< setw(2) << hour << ":" << setfill('0')
+				<< setw(2) << minute << ":" << setfill('0')
+				<< setw(2) << second << " AM in 12-hour format.";
 	}
 }
 
-void twelvetotwentyfour(std::string name) {
+//Function Name: get_value
+//Purpose: The purpose of this function is to ensure that the value entered (hours/minutes/seconds) are valid.
+//Parameter: name of the measurements in the program, maximum amount of the measurement entered, minimum amount of the measurement entered
+//Return: valid measurements
+
+//This function takes integers called upper and lower to define which values are correct depending on the situation.
+int get_value(string name, int lower, int upper) {
+	while (true) {
+		cout << "Please enter a measurement for " << name << ": ";
+		int value;
+		cin >> value;
+		if (value < lower or value > upper) {
+			cout << value << " is not a valid measurement for " << name
+					<< ".";
+			cout << " Please enter a number between " << lower << " and "
+					<< upper << "\n";
+		} else {
+			return value;
+		}
+	}
+}
+
+//Function Name: ampm
+//Purpose: The purpose of this function is to ensure that the information entered is valid.
+//Parameter: name of the measurements in the program.
+//Return: valid type of time (AM or PM)
+
+string ampm(string name) {
+	while (true) {
+		cout << "Please enter either " << name << ": ";
+		string value;
+		cin >> value;
+		if (value != "am" && value != "AM" && value != "Am" && value != "aM"
+				&& value != "pm" && value != "PM" && value != "Pm"
+				&& value != "pM") {
+			cout << value << " is not a valid input for " << name << ".";
+			cout << " Please enter either AM or PM.\n`";
+		} else {
+			return value;
+		}
+	}
+}
+
+//Function Name: twelvetwentyfour
+//Purpose: The purpose of this function is to convert time from 12 to 24 hour notation.
+//Parameter: n/a
+//Return: n/a
+
+void twelvetotwentyfour() {
 	int twelveh_hour, twelveh_minute, twelveh_second;
-	std::string am_pm, time;
-	std::cout
-			<< "This option converts time from 12-hour notation to 24-hour notation.\n"
-					"Please enter in the hour value for the time that will be converted: ";
-	std::cin >> twelveh_hour;
-	std::cout
-			<< "Please enter in the minute value for the time that will be converted: ";
-	std::cin >> twelveh_minute;
-	std::cout
-			<< "Please enter in the second value for the time that will be converted: ";
-	std::cin >> twelveh_second;
-	std::cout << "The time that has been input is " << twelveh_hour << ":"
-			<< twelveh_minute << ":" << twelveh_second
-			<< ". Is it in AM or PM? ";
-	std::cin >> am_pm;
+	string am_pm, time;
+	cout << "This option converts time from 12-hour notation to 24-hour notation.\n";
+	twelveh_hour = get_value("hours", 1, 12);
+	twelveh_minute = get_value("minutes", 0, 59);
+	twelveh_second = get_value("seconds", 0, 59);
+	cout << "The time that has been input is " << setfill('0')
+			<< setw(2) << twelveh_hour << ":" << setfill('0')
+			<< setw(2) << twelveh_minute << ":" << setfill('0')
+			<< setw(2) << twelveh_second << ". Is it in AM or PM? ";
+	am_pm = ampm("AM or PM");
 	output12(twelveh_hour, twelveh_minute, twelveh_second, am_pm);
 }
 
-void twentyfourtotwelve(std::string name) {
-	int twentyfourh_hour, twentyfourh_minute, twentyfourh_second;
-	std::string time;
-	std::cout
-			<< "This option converts time from 12-hour notation to 24-hour notation.\n"
-					"Please enter in the hour value for the time that will be converted: ";
-	std::cin >> twentyfourh_hour;
-	std::cout
-			<< "Please enter in the minute value for the time that will be converted: ";
-	std::cin >> twentyfourh_minute;
-	std::cout
-			<< "Please enter in the second value for the time that will be converted: ";
-	std::cin >> twentyfourh_second;
-	output24(twentyfourh_hour, twentyfourh_minute, twentyfourh_second);
+//Function Name: twentyfourtotwelve
+//Purpose: The purpose of this function is to convert time from 24 to 12 hour notation.
+//Parameter: n/a
+//Return: n/a
 
-// << std::setfill('0') << std::setw(2)
+void twentyfourtotwelve() {
+	int twentyfourh_hour, twentyfourh_minute, twentyfourh_second;
+	string time;
+	cout << "This option converts time from 24-hour notation to 12-hour notation.\n";
+	twentyfourh_hour = get_value("hours", 0, 23);
+	twentyfourh_minute = get_value("minutes", 0, 59);
+	twentyfourh_second = get_value("seconds", 0, 59);
+	output24(twentyfourh_hour, twentyfourh_minute, twentyfourh_second);
 
 }
 
+//Function Name: intro
+//Purpose: The purpose of this function is to display the menu.
+//Parameter: n/a
+//Return: n/a
+
 void intro() {
-	std::cout
+	cout
 			<< "This program is used to convert time from 12-hour notation to 24-hour notation and "
 					"vice versa. \nPlease enter select which option you would like to use:\n"
 					"1. Convert from 12-hour notation to 24-hour notation\n"
 					"2. Convert from 24-hour notation to 12-hour notation\n"
-					"3. Quit program\n";
-
-//        if (answer > 3 or answer < 1) {
-//            std::cout
-//                    << "This is an invalid value. Please enter a number that corresponds to the options given.";
+					"3. Quit program\n\n";
 }
+
+//Function Name: main
+//Purpose: The purpose of this function is to start the program.
+//Parameter: n/a
+//Return: Returns zero or nonzero value which indicates if the program runs smoothly or if there are any problems.
 
 int main() {
 	while (true) {
 		intro();
-		std::cout << "Which select which option you would like to use: ";
-			int answer;
-			std::cin >> answer;
-			if (answer == 1) {
-				twelvetotwentyfour("a");
-			}
-			if (answer == 2) {
-				twentyfourtotwelve("a");
-			}
-			if (answer == 3) {
-				return 0;
-			}
+		cout << "Which select which option you would like to use: ";
+		int answer;
+		cin >> answer;
+		if (answer == 1) {
+			twelvetotwentyfour();
+		}
+		if (answer == 2) {
+			twentyfourtotwelve();
+		}
+		if (answer == 3) {
+			return 0;
+		}
+		if (3 < answer or answer < 1) {
+			cout
+					<< "This is an invalid value. Please enter a number that corresponds to the options given.";
+		}
 
-		std::cout << "\nWould you like to run this program again? (y/n)";
-		std::string yesorno;
-		std::cin >> yesorno;
+		cout << "\n\nWould you like to run this program again? (y/n) ";
+		string yesorno;
+		cin >> yesorno;
 		if (yesorno == "Y" or yesorno == "y") {
+			cout << "\n";
 		}
 		if (yesorno == "N" or yesorno == "n") {
 			break;
@@ -114,26 +190,3 @@ int main() {
 	}
 	return 0;
 }
-//    std::cout
-//            << "This program is used to convert time from 12-hour notation to 24-hour notation and "
-//                    "vice versa. \nPlease enter select which option you would like to use:\n"
-//                    "1. Convert from 12-hour notation to 24-hour notation\n"
-//                    "2. Convert from 24-hour notation to 12-hour notation\n"
-//                    "3. Quit program\n";
-//    std::cout << "Which select which option you would like to use: ";
-//    int answer;
-//    std::cin >> answer;
-//    if (answer == 1) {
-//        answer = twelvetotwentyfour("a");
-//    }
-//    if (answer == 2) {
-//        answer = twentyfourtotwelve("a");
-//    }
-//    if (answer == 3) {
-//        return 0;
-//    }
-//    if (answer > 3 or answer < 1) {
-//        std::cout
-//                << "This is an invalid value. Please enter a number that corresponds to the options given.";
-//    }
-
