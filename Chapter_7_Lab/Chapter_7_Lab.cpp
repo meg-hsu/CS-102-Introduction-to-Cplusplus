bbd5c400d81273618bfca9eaad5c0c70759f7ed7
Meg Hsu <meghsu@Megs-MBP-2.localdomain>
2020-07-07 17:06:33 -0700

    Chapter 7 (Actual) First Working Draft

--- Chapter_7_Lab/Chapter_7_Lab.cpp
+++ Chapter_7_Lab/Chapter_7_Lab.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <assert.h>
 //using namespace std;
 
 enum triangle_types {
@@ -22,7 +21,7 @@ double positivevalue(std::string name) {
 	}
 }
 
-bool nottriangle(double s1, double s2, double s3) {
+bool isitatriangle(double s1, double s2, double s3) {
 	if (s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1) {
 		return true;
 	} else {
@@ -30,75 +29,74 @@ bool nottriangle(double s1, double s2, double s3) {
 	}
 }
 
-bool scalenetriangle(double s1, double s2, double s3) {
-	if (s1 != s2 && s2 != s3 && s3 != s1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-bool isoscelestriangle(double s1, double s2, double s3) {
-	if (s1 == s2 or s2 == s3 or s3 == s1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-bool equilateraltriangle(double s1, double s2, double s3) {
-	if (s1 == s2 && s2 == s3 && s3 == s1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-triangle_types triangle_type(double s1, double s2, double s3) {
-	if (nottriangle(s1, s2, s3) == false) {
+triangle_types triangledef(double s1, double s2, double s3) {
+	if (!isitatriangle(s1, s2, s3)) {
 		return notatriangle;
-	} else if (scalenetriangle(s1, s2, s3) == false) {
+	} else if (s1 != s2 && s2 != s3 && s3 != s1) {
 		return scalene;
-	} else if (isoscelestriangle(s1, s2, s3) == false) {
+	} else if ((s1 == s2 && s2 != s3) or (s1 == s3 && s3 != s2)
+			or (s2 == s3 && s3 != s1)) {
 		return isosceles;
-	} else if (equilateraltriangle(s1, s2, s3) == false) {
+	} else if (s1 == s2 && s2 == s3 && s3 == s1) {
 		return equilateral;
 	} else {
-		std::cout << "There has been an error. Please try again";
-		return 0;
+		std::cout << "Error";
+		return notatriangle;
 	}
 }
 
-void output(double s1, double s2, double s3, triangle_types) {
-	if (notatriangle) {
-		std::cout << "With the measurements" << s1 << ", " << s2 << ", and "
-				<< s3
-				<< "given, it can be determined that these three sides can't form any shape.";
-	}
-	if (scalene) {
-		std::cout << "With the measurements" << s1 << ", " << s2 << ", and "
-				<< s3
-				<< "given, it can be determined that these three sides form a scalene triangle.";
-	}
-	if (isosceles) {
-		std::cout << "With the measurements" << s1 << ", " << s2 << ", and "
-				<< s3
-				<< "given, it can be determined that these three sides form a isosceles triangle.";
-	}
-	if (equilateral) {
-		std::cout << "With the measurements" << s1 << ", " << s2 << ", and "
-				<< s3
-				<< "given, it can be determined that these three sides form a equilateral triangle.";
+void output(triangle_types output);
+
+void triangle_type() {
+	double s1, s2, s3;
+	triangle_types returned;
+	std::cout
+			<< "This program is used to identify types of triangles given the measurements of any three sides.\n";
+	s1 = positivevalue("the first side");
+	s2 = positivevalue("the second side");
+	s3 = positivevalue("the last side");
+	returned = triangledef(s1, s2, s3);
+	output(returned);
+}
+
+void output(triangle_types returned) {
+	switch (returned) {
+	case notatriangle:
+		std::cout
+				<< "With the measurements given, it can be determined that these three sides can't form any shape.";
+		break;
+	case scalene:
+		std::cout
+				<< "With the measurements given, it can be determined that these three sides form a scalene triangle.";
+		break;
+	case isosceles:
+		std::cout
+				<< "With the measurements given, it can be determined that these three sides form a isosceles triangle.";
+		break;
+	case equilateral:
+		std::cout
+				<< "With the measurements given, it can be determined that these three sides form a equilateral triangle.";
+		break;
+	default:
+		std::cerr << "Error: unknown triangle type " << returned << std::endl;
 	}
 }
 
 int main() {
-	while (true)
+	while (true) {
 		double s1, s2, s3;
-	triangle_types triangletype;
-	double s1 = positivevalue("the first side of the triangle");
-	double s2 = positivevalue("the second side of the triangle");
-	double s3 = positivevalue("the third side of the triangle");
-	triangletype = triangle_type(s1, s2, s3);
-	output(s1, s2, s3, triangletype);
+		triangle_types returned;
+		triangle_type();
+
+		std::cout << "\n\nWould you like to run this program again? (y/n) ";
+		std::string yesorno;
+		std::cin >> yesorno;
+		if (yesorno == "Y" or yesorno == "y") {
+			std::cout << "\n";
+		}
+		if (yesorno == "N" or yesorno == "n") {
+			break;
+		}
+	}
 	return 0;
 }
